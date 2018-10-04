daac2452813979b1353cb9c9cde2fc275a086295
itcutlet <itcutlet@gmail.com>
2018-10-04 16:23:11 +0300

    Init

--- src/ExceptionInterface.php
+++ src/ExceptionInterface.php
@@ -6,14 +6,9 @@ namespace luckyshopteam\sentry;
 interface ExceptionInterface
 {
     public function ready(): bool;
-
     public function getTags(): array;
-
     public function getExtra(): array;
-
     public function setReady(bool $ready);
-
     public function addTag(string $tag, $value);
-
     public function addExtra(string $name, $value);
 }
